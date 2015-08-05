b0588f1d1855e2611bc8ad651c65fc1305478f94
Mikhail Laptev <mikhail.laptev@gmail.com>
2015-08-05 16:40:39 +0300

    additional set of functions for training

--- mera/practice_with_function/callable_instance.py
+++ mera/practice_with_function/callable_instance.py
@@ -5,3 +5,15 @@ Created on Aug 3, 2015
 
 @summary: Create callable instance of the class
 '''
+
+class callable_class:
+    def __init__(self):
+        self.function_to_call = lambda : "Call from the class {}".format(self.__class__.__name__) 
+    
+    def __call__(self):
+        return self.function_to_call()
+    
+
+if __name__ == "__main__":
+    try_callable_class = callable_class()
+    print try_callable_class()
--- mera/practice_with_function/transpose_2d_array.py
+++ mera/practice_with_function/transpose_2d_array.py
@@ -5,3 +5,23 @@ Created on Aug 3, 2015
 
 @summary: Transpose 2D array of lists
 '''
+
+# One agreement that all sub-lists has the same length
+def transpose_2D(matrix):
+    """
+    >>> transpose_2D([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
+    [[1, 5, 9], [2, 6, 10], [3, 7, 11], [4, 8, 12]]
+    """
+    return [[matrix[i][j] for i in range(len(matrix))] for j in range(len(matrix[0]))]
+
+def transpose_2D_with_map(matrix):
+    """
+    >>> transpose_2D_with_map([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
+    [[1, 5, 9], [2, 6, 10], [3, 7, 11], [4, 8, 12]]
+    """
+    print [[1], [2], [3], [4]], [[1], [2], [3], [4]] 
+    return map(lambda *i: list(i), *matrix)
+
+if __name__ == "__main__":
+    import doctest
+    print doctest.testmod()
