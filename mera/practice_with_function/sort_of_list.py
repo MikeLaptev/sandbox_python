cb2f9463c1e1c54d043e222bdd9eeec6868581d5
Mikhail Laptev <mikhail.laptev@gmail.com>
2015-08-06 21:23:15 +0300

    Performance measurements has been added

--- mera/practice_with_function/sort_of_list.py
+++ mera/practice_with_function/sort_of_list.py
@@ -13,6 +13,20 @@ def sort_by_latest_letter(list_of_strings):
     """
     return [sorted_element[::-1] for sorted_element in sorted([element[::-1] for element in list_of_strings])]
 
+def sort_by_latest_letter_with_map(list_of_strings):
+    """
+    >>> sort_by_latest_letter_with_map(["abc", "cab", "bca"])
+    ['bca', 'cab', 'abc']
+    """
+    return map(lambda x: x[::-1], *[sorted(map(lambda a: a[::-1], list_of_strings))])
+
 if __name__ == "__main__":
+    # acceptance testing
     import doctest
     print doctest.testmod()
+    
+    # performance testing
+    import timeit
+    first_solution = timeit.timeit(stmt='sort_by_latest_letter(["abc", "cab", "bca"])', setup="from __main__ import sort_by_latest_letter")
+    second_solution = timeit.timeit(stmt='sort_by_latest_letter_with_map(["abc", "cab", "bca"])', setup="from __main__ import sort_by_latest_letter_with_map")
+    print "First solution took {}, but second solution took {}".format(first_solution, second_solution)
