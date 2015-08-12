9ea441522c91384e494d7c9c7e7a1194adb492ba
Mikhail Laptev <mikhail.laptev@gmail.com>
2015-08-12 12:23:19 +0300

    Additional comments has been implemented
    Several comments has been implemented. All about sum specific Python's
    features that should be used instead of common/straightforward approach.

--- mera/iterators_and_generators/file_processing_generator.py
+++ mera/iterators_and_generators/file_processing_generator.py
@@ -4,14 +4,30 @@ Created on Aug, 11 2015
 @author: mlaptev
 '''
 
+# Simple implementation
 def file_processing(file_name = "TCP_IP.txt", word_to_search="TCP"):
     string_number = 0
     with open(file_name, 'r') as f:
         for line_in_file in f:
             string_number += 1
             if word_to_search in line_in_file:
-                yield "{}: {}{}".format(string_number, line_in_file[:68], "...") 
+                yield "{}: {}".format(string_number, line_in_file if len(line_in_file) <= 68 else "{}{}".format(line_in_file[:68], "...")) 
+                
+# Implementation with usage of Python's features
+def file_processing_with_more_python_features(file_name = "TCP_IP.txt", word_to_search="TCP"):
+    with open(file_name, 'r') as f:
+        for string_number, line_in_file in enumerate(f, 1):
+            if word_to_search in line_in_file:
+                yield "{}: {}".format(string_number, line_in_file if len(line_in_file) <= 68 else "{}{}".format(line_in_file[:68], "...")) 
 
 if __name__ == '__main__':
+    # first solution
+    print ">>> First Solution <<<"
     for line_info in file_processing():
+        print line_info
+    
+    # second solution
+    print 
+    print ">>> Second Solution <<<"
+    for line_info in file_processing_with_more_python_features():
         print line_info
--- mera/iterators_and_generators/sum_of_cubes.py
+++ mera/iterators_and_generators/sum_of_cubes.py
@@ -3,6 +3,7 @@ Created on Aug, 11 2015
 
 @author: mlaptev
 '''
+import time
 
 def generator_of_sum_of_cubes(start, finish):
     pointer = start
@@ -11,8 +12,17 @@ def generator_of_sum_of_cubes(start, finish):
         pointer += 1
 
 if __name__ == '__main__':
-    sum = 0
-    prev_sum = 0
+    print ">>> First Solution <<<"
+    start_time = time.time()
+    sum_f = 0
     for i in generator_of_sum_of_cubes(1, 3000000):
-        sum += i
-    print "Result sum is: {}".format(sum)
+        sum_f += i
+    finish_time = time.time()
+    print "Result sum is: {}. Working time: {}".format(sum_f, finish_time - start_time)
+    
+    print ">>> Second Solution <<<"
+    start_time = time.time()
+    sum_s = sum(value for value in generator_of_sum_of_cubes(1, 3000000))
+    finish_time = time.time()
+    print "Result sum is: {}. Working time: {}".format(sum_s, finish_time - start_time)
+    
