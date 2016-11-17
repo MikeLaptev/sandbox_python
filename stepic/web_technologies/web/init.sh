925d958a6bf7d46ae5346578202a4ce3cfd277b7
Mikhail Laptev <mikhail.laptev@gmail.com>
2016-11-17 19:47:56 +0100

    Updated files

--- stepic/web_technologies/web/init.sh
+++ stepic/web_technologies/web/init.sh
@@ -1,4 +1,4 @@
-rm -f /etc/nginx/sites-enabled/default
+sudo rm -f /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-rm -f /etc/nginx/sites-enabled/default
+sudo rm -f /etc/nginx/sites-enabled/default
